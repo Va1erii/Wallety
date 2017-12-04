7ebfd7c962db484ef10d313ad520084204f3441a
Va1erii <valerapopovsakha@gmail.com>
2017-12-04 21:38:43 +0300

    Main Activity

---
+++ app/src/main/java/com/valeriipopov/wallety/CardViewActivity.java
@@ -0,0 +1,13 @@
+package com.valeriipopov.wallety;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CardViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_view);
+    }
+}
---
+++ app/src/main/java/com/valeriipopov/wallety/MainActivity.java
@@ -0,0 +1,29 @@
+package com.valeriipopov.wallety;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRecyclerView = findViewById(R.id.my_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new MyAdapter (myDataSet);
+        mRecyclerView.setAdapter(mAdapter);
+
+    }
+}
