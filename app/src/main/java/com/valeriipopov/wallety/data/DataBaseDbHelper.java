3a67bfabb1fe9084db28f0ab4b2d09f0d006920c
Va1erii <valerapopovsakha@gmail.com>
2017-12-11 22:11:46 +0300

    Added new config defaultSupportLibrary

--- app/src/main/java/com/valeriipopov/wallety/data/DataBaseDbHelper.java
+++ app/src/main/java/com/valeriipopov/wallety/data/DataBaseDbHelper.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.widget.EditText;
 
 import com.valeriipopov.wallety.Item;
